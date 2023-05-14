0c66f788385bef922c38b99253c9859f10952c23
Simon Mårtensson <martensson.simon@gmail.com>
2023-05-14 13:19:37 +0200

    Update

--- gen-bootable.sh
+++ gen-bootable.sh
@@ -1,66 +1,103 @@
-# Create empty image
-qemu-img create kernel.img 2M
+#!/bin/sh
 
-# Partition the image with MBR partioning
+
+# Define variables
+IMAGE_PATH="./kernel.img"
+IMAGE_SIZE="8M"
+MOUNT_DIR="/mnt"
+
+DISK_GEOMETRY_HEADS="16"
+DISK_GEOMETRY_SECTORS="63"
+DISK_GEOMETRY_CYLINDERS="4"
+
+PARTITION_FILE_SYSTEM="ext2"
+PARITION_SECTOR_OFFSET="2048"
+PARTITION_BYTES_OFFSET=$(( $PARITION_SECTOR_OFFSET * 512 ))
+
+LOOP_DEV="/dev/loop0"
+LOOP_DEV_PART="/dev/loop1"
+
+
+# Create empty image
+qemu-img create "$IMAGE_PATH" "$IMAGE_SIZE"
+
+
+# Partition the image with fdisk with MBR (Master Boot Record) partition table
 # 1. Define disk geometry: 16 heads (max), 63 sectors (max) but only 4 cylinders to keep the image within 2MB (512 * 16 * 63 * 4 = 2 064 384 bytes)
 # 2. Create a new primary partition with start sector 2048 (normal for GRUB) and end sector 4095
 # 3. Set boot flag to the new partition to indicate that the device is bootable. Needed for BIOS to find the correct device to boot from
-fdisk ./kernel.img << EOF
+fdisk "$IMAGE_PATH" << EOF
 x
 c
-4
+$DISK_GEOMETRY_CYLINDERS
 h
-16
+$DISK_GEOMETRY_HEADS
 s
-63
+$DISK_GEOMETRY_SECTORS
 r
 n
 p
 1
-2048
-4095
+$PARITION_SECTOR_OFFSET
+
 a
 w
 EOF
 
-# Print image layout
-fdisk -l -u ./kernel.img
+fdisk -l -u "$IMAGE_PATH"
 
-sudo losetup -o 1048576 /dev/loop1 ./kernel.img
-sudo mke2fs -t ext2 /dev/loop1
-echo "Created loop device and formatted partition (EXT2)"
 
-sudo mount /dev/loop1 /mnt/
-echo "Mounted loop device"
+# Associate the image with a loop device and create a partition device
+echo "-> Attaching loop devices"
+losetup "$LOOP_DEV" "$IMAGE_PATH"
+losetup -o $PARTITION_BYTES_OFFSET "$LOOP_DEV_PART" "$IMAGE_PATH"
+sleep 0.1
 
-cp -R bootdisk/* /mnt/
-cp kernel/bin/simux_kernel.elf /mnt/boot/
-echo "Copied GRUB configuration files and kernel to filesystem"
 
-# sudo grub-install --root-directory=/mnt /dev/loop0
-# sudo grub-mkconfig -o /mnt/boot/grub/grub.cfg
+# Format the partition
+echo "-> Formatting partition"
+mke2fs -t "$PARTITION_FILE_SYSTEM" "$LOOP_DEV_PART"
 
-ls -lahR /mnt/
 
-sudo umount /mnt/
-echo "Unmounted loop device"
+# Mount the partition
+echo "-> Mounting partition"
+mount "$LOOP_DEV_PART" "$MOUNT_DIR"
+rm -rf "$MOUNT_DIR"/*
 
-return
 
-sudo grub --device-map=/dev/null << EOF
-device (hd0) ./kernel.img
-geometry (hd0) 4 16 63
-root (hd0,0)
-kernel /boot/simux_kernel.elf root=/dev/hda
-setup (hd0)
-quit
+echo "-> Install GRUB"
+mkdir -p /mnt/boot/grub
+
+grub-install --target i386-pc --no-floppy --root-directory=/mnt /dev/loop0
+#cp -R bootdisk/* /mnt/
+cat > /mnt/boot/grub/device.map <<EOF
+(hd0)   /dev/loop0
+(hd0,1) /dev/loop1
 EOF
-echo "GRUB installed!"
 
-sudo losetup -d /dev/loop1
-echo "Removed loop device"
 
-echo "Done!"
+echo "-> Copying kernel"
+cp kernel/bin/simux_kernel.elf /mnt/boot/
+cat /mnt/boot/grub/device.map
+rm -rf /mnt/boot/grub/device.map
+
+ls -lah /mnt/boot/grub
+
+
+echo "-> Detaching loop devices..."
+umount /mnt/
+sleep 0.1
+losetup -d /dev/loop1
+sleep 0.1
+kpartx -v -d /dev/loop0
+sleep 0.1
+losetup -d /dev/loop0
+sleep 0.1
+
+echo "-> MD5 checksum:"
+md5sum kernel.img
+
+echo "-> Done!"
 
 
 
--- qemu.sh
+++ qemu.sh
@@ -1,1 +1,7 @@
-qemu-system-i386 -m 1024 -s -hda ./kernel.img  -curses -serial /dev/tty  -redir tcp:2323::23
+qemu-system-i386 -m 1024 -s -drive format=raw,file=./kernel.img -curses -serial /dev/tty  -redir tcp:2323::23
+#qemu-system-i386 -m 1024 -s -hda ./kernel.img  -curses -serial /dev/tty  -redir tcp:2323::23
+
+
+# KNOWN TO WORK
+# qemu-system-i386 -m 1024 -s -hda ./linux-0.2.img  -curses -serial /dev/tty  -redir tcp:2323::23
+# qemu-system-i386 -m 1024 -s -drive format=raw,file=./kernel.img -curses -serial /dev/tty  -redir tcp:2323::23
