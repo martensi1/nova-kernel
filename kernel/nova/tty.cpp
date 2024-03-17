e15bacd9099c52b0a9f527710f71c16d413b80bd
Simon Mårtensson <martensson.simon@gmail.com>
2024-03-17 23:51:40 +0100

    Added improved comments

--- kernel/nova/tty.cpp
+++ kernel/nova/tty.cpp
@@ -1,3 +1,26 @@
+////////////////////////////////////////////////////////////
+//
+// Nova OS
+// Copyright (C) 2024 Simon Mårtensson
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+////////////////////////////////////////////////////////////
 #include <nova/tty.h>
 #include <nova/common.h>
 #include <nova/console/driver.h>
@@ -26,6 +49,7 @@ namespace Nova
             }
         }
 
+        ////////////////////////////////////////////////////////////
         static FORCE_INLINE void writeData(const char* data, size_t size)
         {
             for (size_t i = 0; i < size; i++) {
@@ -42,6 +66,7 @@ namespace Nova
             activeDriver->updateCursor();
         }
 
+        ////////////////////////////////////////////////////////////
         static FORCE_INLINE void writeData(const char* data)
         {
             size_t length = strlen(data);
