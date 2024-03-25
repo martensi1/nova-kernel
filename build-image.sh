d74f726c79a251a8c7d6be6b722edd2230b37851
Simon Alm-Mårtensson <martensson.simon@gmail.com>
2024-03-25 09:46:58 +0100

    Update build script version

--- build-image.sh
+++ build-image.sh
@@ -44,7 +44,7 @@ error () {
 ############################
 # Print welcome message
 ############################
-VERSION='0.1.0'
+VERSION='0.1.1'
 
 echo "############################################"
 echo "#    Nova OS - Image Builder (v${VERSION})  "
