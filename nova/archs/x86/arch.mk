e03a1658c6fa8e11f02fa7a29a9da7d0864e08d0
Simon Alm-Mårtensson <martensson.simon@gmail.com>
2024-03-31 22:06:33 +0200

    Improvement

--- nova/archs/x86/arch.mk
+++ nova/archs/x86/arch.mk
@@ -1,2 +1,13 @@
 include archs/x86/boot/module.mk
 include archs/x86/kernel/module.mk
+
+
+ifeq ($(ARCH), i386)
+	CXXFLAGS += -m32 -DNOVA_ARCH_I386=1
+	ASFLAGS += -f elf32
+	LDFLAGS += -m elf_i386
+else
+	CXXFLAGS += -m32 -DNOVA_ARCH_X86_64=1
+	ASFLAGS += -f elf32
+	LDFLAGS += -m elf_i386
+endif
