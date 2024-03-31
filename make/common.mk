a2282ff66e1033a006c9a14d27bf88374c0eb04c
Simon Alm-Mårtensson <martensson.simon@gmail.com>
2024-03-31 23:21:19 +0200

    Update

--- make/common.mk
+++
@@ -1,9 +0,0 @@
-nova_define = $(eval $1 := $2)$(eval export $1)
-
-source-to-object = $(subst .cpp,.o,$(filter %.cpp,$1)) \
-                   $(subst .asm,.o,$(filter %.asm,$1)) \
-				   $(subst .c,.o,$(filter %.c,$1))
-
-subdirectory = $(patsubst %/module.mk,%,                        \
-                 $(word                                         \
-                   $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))
--- make/system.mk
+++
@@ -1,7 +0,0 @@
-nova_define = $(eval $1 := $2)$(eval export $1)
-
-nova_define(CXX, g++)
-nova_define(AS, nasm)
-nova_define(MV, mv -f)
-nova_define(RM, rm -f)
-nova_define(SED, sed)
