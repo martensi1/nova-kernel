9d56c299d69139c9276aed7a44bda62bc2a0c164
Simon Alm-Mårtensson <martensson.simon@gmail.com>
2024-04-03 00:19:28 +0200

    Update

---
+++ nova/arch-map.mk
@@ -0,0 +1,5 @@
+# This file is used to map the architecture to the corresponding directory
+# in the 'archs' folder. This is used to include the correct architecture
+# flags and source files when building the kernel
+ARCH_DIR_i386 := x86
+ARCH_DIR_x86_64 := x86
